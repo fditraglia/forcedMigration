3848e17b66d8dd48069773e54355d0822a809302
Francis DiTraglia <fditra@sas.upenn.edu>
2016-10-18 22:07:37 +0200

    add function to calculate joint surplus for paramilitaries and elites, clean and rebuild

--- src/GetEquilibrium.cpp
+++ src/GetEquilibrium.cpp
@@ -1,5 +1,6 @@
 // [[Rcpp::depends(RcppEigen)]]
 // [[Rcpp::depends(RcppNumerical)]]
+
 #include <RcppNumerical.h>
 using namespace Numer;
 
@@ -30,8 +31,8 @@ class MigrationIntegrand: public Func
       double f_c = R::dlnorm(h, mu_h, s_h, 0);
       return F_c * f_c;
     }
-
 };
+
 
 // [[Rcpp::export]]
 double get_Dstar(double D_e, double V, double r, double P, double s_h, double mu_h,
@@ -51,13 +52,14 @@ double get_Dstar(double D_e, double V, double r, double P, double s_h, double mu
   const double D_n = (1 - delta) * R::plnorm(tau_n * Q, mu_c, s_c, 1, 0);
 
   return frac_n * D_n + (1 - frac_n) * D_ell;
-
 }
+
 
 // [[Rcpp::export]]
 double get_migration_eq(double V, double r, double P, double s_h, double mu_h,
                   double tau_ell, double s_c, double mu_c, double tau_n,
-                  double delta, double frac_n){
+                  double delta, double frac_n)
+{
   const int max_iter = 500;
   const double tol = 0.0001;
   int i = 0;
@@ -71,3 +73,47 @@ double get_migration_eq(double V, double r, double P, double s_h, double mu_h,
   return(x);
 }
 
+class ViolenceIntegrand: public Func
+{
+  private:
+    double Dstar;
+    double V;
+    double r;
+    double P;
+    double s_h;
+    double mu_h;
+    double tau_ell;
+    double s_c;
+    double mu_c;
+  public:
+    ViolenceIntegrand(double Dstar_, double V_, double r_, double P_,
+                      double s_h_, double mu_h_, double tau_ell_,
+                      double s_c_, double mu_c_) : Dstar(Dstar_), V(V_), r(r_),
+                                                   P(P_), s_h(s_h_),
+                                                   mu_h(mu_h_),
+                                                   tau_ell(tau_ell_),
+                                                   s_c(s_c_), mu_c(mu_c_) {}
+    double operator()(const double& h) const
+    {
+      const double Q = V / (P * (1 - Dstar));
+      double F_c = R::plnorm(tau_ell * Q - r * h, mu_c, s_c, 1, 0);
+      double f_c = R::dlnorm(h, mu_h, s_h, 0);
+      return h * F_c * f_c;
+    }
+};
+
+// [[Rcpp::export]]
+double get_surplus(double V, double r, double P, double s_h, double mu_h,
+                   double tau_ell, double s_c, double mu_c, double tau_n,
+                   double delta, double frac_n, double gamma, double V_cost)
+{
+  double Dstar = get_migration_eq(V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, tau_n,
+                                  delta, frac_n);
+  ViolenceIntegrand g(Dstar, V, r, P, s_h, mu_h, tau_ell, s_c, mu_c);
+  double err_est;
+  int err_code;
+  double upper = tau_ell * (V / (P * (1 - Dstar))) / r;
+  const double res = integrate(g, 0.0, upper, err_est, err_code);
+  double Xstar = (1 - delta) * res;
+  return Xstar - gamma * Dstar - V_cost * V;
+}
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -49,3 +49,26 @@ BEGIN_RCPP
     return __result;
 END_RCPP
 }
+// get_surplus
+double get_surplus(double V, double r, double P, double s_h, double mu_h, double tau_ell, double s_c, double mu_c, double tau_n, double delta, double frac_n, double gamma, double V_cost);
+RcppExport SEXP forcedMigration_get_surplus(SEXP VSEXP, SEXP rSEXP, SEXP PSEXP, SEXP s_hSEXP, SEXP mu_hSEXP, SEXP tau_ellSEXP, SEXP s_cSEXP, SEXP mu_cSEXP, SEXP tau_nSEXP, SEXP deltaSEXP, SEXP frac_nSEXP, SEXP gammaSEXP, SEXP V_costSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< double >::type V(VSEXP);
+    Rcpp::traits::input_parameter< double >::type r(rSEXP);
+    Rcpp::traits::input_parameter< double >::type P(PSEXP);
+    Rcpp::traits::input_parameter< double >::type s_h(s_hSEXP);
+    Rcpp::traits::input_parameter< double >::type mu_h(mu_hSEXP);
+    Rcpp::traits::input_parameter< double >::type tau_ell(tau_ellSEXP);
+    Rcpp::traits::input_parameter< double >::type s_c(s_cSEXP);
+    Rcpp::traits::input_parameter< double >::type mu_c(mu_cSEXP);
+    Rcpp::traits::input_parameter< double >::type tau_n(tau_nSEXP);
+    Rcpp::traits::input_parameter< double >::type delta(deltaSEXP);
+    Rcpp::traits::input_parameter< double >::type frac_n(frac_nSEXP);
+    Rcpp::traits::input_parameter< double >::type gamma(gammaSEXP);
+    Rcpp::traits::input_parameter< double >::type V_cost(V_costSEXP);
+    __result = Rcpp::wrap(get_surplus(V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, tau_n, delta, frac_n, gamma, V_cost));
+    return __result;
+END_RCPP
+}
