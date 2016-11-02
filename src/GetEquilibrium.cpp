f19d6816cb795a7d38a4889f470f8a273131c301
Francis DiTraglia <fditra@sas.upenn.edu>
2016-11-02 11:11:26 -0400

    allow correlation between migration costs and landholdings

--- src/GetEquilibrium.cpp
+++ src/GetEquilibrium.cpp
@@ -16,31 +16,39 @@ class MigrationIntegrand: public Func
     double tau_ell;
     double s_c;
     double mu_c;
+    double rho;
   public:
     MigrationIntegrand(double D_e_, double V_, double r_, double P_,
                        double s_h_, double mu_h_, double tau_ell_,
-                       double s_c_, double mu_c_) : D_e(D_e_), V(V_), r(r_),
-                                                    P(P_), s_h(s_h_),
-                                                    mu_h(mu_h_),
-                                                    tau_ell(tau_ell_),
-                                                    s_c(s_c_), mu_c(mu_c_) {}
+                       double s_c_, double mu_c_, double rho_) : D_e(D_e_),
+                                                                 V(V_), r(r_),
+                                                                 P(P_),
+                                                                 s_h(s_h_),
+                                                                 mu_h(mu_h_),
+                                                                 tau_ell(tau_ell_),
+                                                                 s_c(s_c_),
+                                                                 mu_c(mu_c_),
+                                                                 rho(rho_) {}
     double operator()(const double& h) const
     {
       const double Q = V / (P * (1 - D_e));
-      double F_c = R::plnorm(tau_ell * Q - r * h, mu_c, s_c, 1, 0);
+      double mu_c_given_h = mu_c + rho * (s_c / s_h) * (log(h) - mu_h);
+      double s_c_given_h = s_c * sqrt(1 - pow(rho, 2));
+      double F_c_given_h = R::plnorm(tau_ell * Q - r * h, mu_c_given_h,
+                                     s_c_given_h, 1, 0);
       double f_h = R::dlnorm(h, mu_h, s_h, 0);
-      return F_c * f_h;
+      return F_c_given_h * f_h;
     }
 };
 
 
 // [[Rcpp::export]]
-double get_Dstar(double D_e, double V, double r, double P, double s_h, double mu_h,
-             double tau_ell, double s_c, double mu_c, double tau_n,
-             double delta, double frac_n)
+double get_Dstar(double D_e, double V, double r, double P, double s_h,
+                 double mu_h, double tau_ell, double s_c, double mu_c,
+                 double rho, double tau_n, double delta, double frac_n)
 {
   // Landholding families
-  MigrationIntegrand g(D_e, V, r, P, s_h, mu_h, tau_ell, s_c, mu_c);
+  MigrationIntegrand g(D_e, V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, rho);
   double err_est;
   int err_code;
   double upper = tau_ell * (V / (P * (1 - D_e))) / r;
@@ -57,17 +65,18 @@ double get_Dstar(double D_e, double V, double r, double P, double s_h, double mu
 
 // [[Rcpp::export]]
 double get_migration_eq(double V, double r, double P, double s_h, double mu_h,
-                  double tau_ell, double s_c, double mu_c, double tau_n,
-                  double delta, double frac_n)
+                        double tau_ell, double s_c, double mu_c, double rho,
+                        double tau_n, double delta, double frac_n)
 {
   const int max_iter = 500;
   const double tol = 0.0001;
   int i = 0;
   double x = 0.0;
-  double f = get_Dstar(x, V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, tau_n, delta, frac_n);
+  double f = get_Dstar(x, V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, rho, tau_n,
+                       delta, frac_n);
   while((i < max_iter) & (std::abs(f - x) > tol)){
     x = f;
-    f = get_Dstar(x, V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, tau_n, delta, frac_n);
+    f = get_Dstar(x, V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, rho, tau_n, delta, frac_n);
     i++;
   }
   return(x);
@@ -85,32 +94,39 @@ class ViolenceIntegrand: public Func
     double tau_ell;
     double s_c;
     double mu_c;
+    double rho;
   public:
     ViolenceIntegrand(double Dstar_, double V_, double r_, double P_,
                       double s_h_, double mu_h_, double tau_ell_,
-                      double s_c_, double mu_c_) : Dstar(Dstar_), V(V_), r(r_),
-                                                   P(P_), s_h(s_h_),
-                                                   mu_h(mu_h_),
-                                                   tau_ell(tau_ell_),
-                                                   s_c(s_c_), mu_c(mu_c_) {}
+                      double s_c_, double mu_c_, double rho_) : Dstar(Dstar_),
+                                                                V(V_), r(r_),
+                                                                P(P_), s_h(s_h_),
+                                                                mu_h(mu_h_),
+                                                                tau_ell(tau_ell_),
+                                                                s_c(s_c_),
+                                                                mu_c(mu_c_),
+                                                                rho(rho_) {}
     double operator()(const double& h) const
     {
       const double Q = V / (P * (1 - Dstar));
-      double F_c = R::plnorm(tau_ell * Q - r * h, mu_c, s_c, 1, 0);
+      double mu_c_given_h = mu_c + rho * (s_c / s_h) * (log(h) - mu_h);
+      double s_c_given_h = s_c * sqrt(1 - pow(rho, 2));
+      double F_c_given_h = R::plnorm(tau_ell * Q - r * h, mu_c_given_h,
+                                     s_c_given_h, 1, 0);
       double f_h = R::dlnorm(h, mu_h, s_h, 0);
-      return h * F_c * f_h;
+      return h * F_c_given_h * f_h;
     }
 };
 
 // [[Rcpp::export]]
 double get_surplus(double V, double r, double P, double s_h, double mu_h,
-                   double tau_ell, double s_c, double mu_c, double tau_n,
-                   double delta, double frac_n, double gamma, double alpha,
-                   double beta)
+                   double tau_ell, double s_c, double mu_c, double rho,
+                   double tau_n, double delta, double frac_n, double gamma,
+                   double alpha, double beta)
 {
-  double Dstar = get_migration_eq(V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, tau_n,
-                                  delta, frac_n);
-  ViolenceIntegrand g(Dstar, V, r, P, s_h, mu_h, tau_ell, s_c, mu_c);
+  double Dstar = get_migration_eq(V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, rho,
+                                  tau_n, delta, frac_n);
+  ViolenceIntegrand g(Dstar, V, r, P, s_h, mu_h, tau_ell, s_c, mu_c, rho);
   double err_est;
   int err_code;
   double upper = tau_ell * (V / (P * (1 - Dstar))) / r;
