d6972c910b8762dc0a0484c7866d23b8bbae04aa
Francis DiTraglia <fditra@sas.upenn.edu>
2016-12-22 12:13:57 -0500

    remove parameter delta (fraction who never migrate) since it is no longer needed

--- src/GetEquilibrium.cpp
+++ src/GetEquilibrium.cpp
@@ -31,7 +31,7 @@ class MigrationIntegrand: public Func
       double upper = (exp(tau_ell * Q - r * h) - 1) * exp(-u_bar);
       double log_F_c_given_h = R::pbeta(upper, a0 + a1 * h, 1, 1, 1);
       double log_f_h = (p - 1) * log(h) + (q - 1) * log(H - h) -
-        R::lbeta(p, q) - (p + q) * log(H);
+        R::lbeta(p, q) - (p + q - 1) * log(H);
       return exp(log_F_c_given_h + log_f_h);
     }
 };
@@ -40,7 +40,7 @@ class MigrationIntegrand: public Func
 // [[Rcpp::export]]
 double get_Dstar(double D_e, double V, double Gamma, double tau_ell,
                  double tau_n, double r, double u_bar, double a0, double a1,
-                 double p, double q, double H, double delta, double omega_n)
+                 double p, double q, double H, double omega_n)
 {
   // Landholding families
   MigrationIntegrand g(D_e, V, Gamma, tau_ell, r, u_bar, a0, a1, p, q, H);
@@ -48,12 +48,11 @@ double get_Dstar(double D_e, double V, double Gamma, double tau_ell,
   int err_code;
   const double Q = get_Q(V, D_e, Gamma);
   const double landed_upper = tau_ell * Q / r;
-  const double res = integrate(g, 0.0, landed_upper, err_est, err_code);
-  const double D_ell = (1 - delta) * res;
+  const double D_ell = integrate(g, 0.0, landed_upper, err_est, err_code);
 
   // Landless Families
   const double landless_upper = (exp(tau_n * Q) - 1) * exp(-u_bar);
-  const double D_n = (1 - delta) * R::pbeta(landless_upper, a0, 1, 1, 0);
+  const double D_n =  R::pbeta(landless_upper, a0, 1, 1, 0);
   return omega_n * D_n + (1 - omega_n) * D_ell;
 }
 
@@ -61,19 +60,16 @@ double get_Dstar(double D_e, double V, double Gamma, double tau_ell,
 // [[Rcpp::export]]
 double get_migration_eq(double V, double Gamma, double tau_ell,
                         double tau_n, double r, double u_bar, double a0,
-                        double a1, double p, double q, double H, double delta,
-                        double omega_n)
+                        double a1, double p, double q, double H, double omega_n)
 {
   const int max_iter = 500;
   const double tol = 0.0001;
   int i = 0;
   double x = 0.0;
-  double f = get_Dstar(x, V, Gamma, tau_ell, tau_n, r, u_bar, a0, a1, p, q, H,
-                  delta, omega_n);
+  double f = get_Dstar(x, V, Gamma, tau_ell, tau_n, r, u_bar, a0, a1, p, q, H, omega_n);
   while((i < max_iter) & (std::abs(f - x) > tol)){
     x = f;
-    f = get_Dstar(x, V, Gamma, tau_ell, tau_n, r, u_bar, a0, a1, p, q, H,
-                  delta, omega_n);
+    f = get_Dstar(x, V, Gamma, tau_ell, tau_n, r, u_bar, a0, a1, p, q, H, omega_n);
     i++;
   }
   return(x);
@@ -103,7 +99,7 @@ class ExpropriationIntegrand: public Func
       double upper = (exp(tau_ell * Q - r * h) - 1) * exp(-u_bar);
       double log_F_c_given_h = R::pbeta(upper, a0 + a1 * h, 1, 1, 1);
       double log_f_h = (p - 1) * log(h) + (q - 1) * log(H - h) -
-        R::lbeta(p, q) - (p + q) * log(H);
+        R::lbeta(p, q) - (p + q - 1) * log(H);
       return exp(log(h) + log_F_c_given_h + log_f_h);
     }
 };
@@ -111,17 +107,16 @@ class ExpropriationIntegrand: public Func
 // [[Rcpp::export]]
 double get_surplus(double V, double Gamma, double tau_ell, double tau_n,
                    double r, double u_bar, double a0, double a1, double p,
-                   double q, double H, double delta, double omega_n,
-                   double gamma, double beta)
+                   double q, double H, double omega_n, double gamma, double beta)
 {
   double Dstar = get_migration_eq(V, Gamma, tau_ell, tau_n, r, u_bar, a0,
-                        a1, p, q, H, delta, omega_n);
+                        a1, p, q, H, omega_n);
   ExpropriationIntegrand g(Dstar, V, Gamma, tau_ell, r, u_bar, a0, a1, p, q, H);
   double err_est;
   int err_code;
   const double Q = get_Q(V, Dstar, Gamma);
   const double upper = tau_ell * Q / r;
   const double res = integrate(g, 0.0, upper, err_est, err_code);
-  double Xstar = (1 - delta) * (1 - omega_n) * res;
+  double Xstar = (1 - omega_n) * res;
   return Xstar - gamma * Dstar - beta;
 }
