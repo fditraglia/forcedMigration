e9d15447ca0263257c02897cef1d457df4ffb4e0
Francis DiTraglia <fditra@sas.upenn.edu>
2017-02-18 11:18:15 -0500

    remove cluster stuff since we're now going to stick to mclapply

--- R/solve_model.R
+++ R/solve_model.R
@@ -1,6 +1,6 @@
 solve_equilibrium_violence <- function(delta, tau_ell, tau_n, r, a0, a1,
                                        land_parameters_list, gamma, alpha,
-                                       cluster = NULL, n_cores = 1){
+                                       n_cores = 1){
 
   migration_params <- list(delta = delta, tau_ell = tau_ell, tau_n = tau_n,
                            r = r, a0 = a0, a1 = a1)
@@ -10,21 +10,15 @@ solve_equilibrium_violence <- function(delta, tau_ell, tau_n, r, a0, a1,
     params <- c(migration_params, surplus_params, land_parameters_list[[i]])
     do.call(get_V_star, params)
   }
-
-  if(!is.null(cluster)){
-    V_total <- parallel::clusterApplyLB(cluster, seq_along(land_parameters_list),
-                                   get_V_star_i)
-  } else {
-    V_total <- parallel::mclapply(seq_along(land_parameters_list),
-                                  get_V_star_i, mc.cores = n_cores)
-  }
+  V_total <- parallel::mclapply(seq_along(land_parameters_list),
+                                get_V_star_i, mc.cores = n_cores)
   return(unlist(V_total))
 }
 
 
 solve_equilibrium_migration_flow <- function(V_cum_list, delta, tau_ell, tau_n,
                                              r, a0, a1, land_parameters_list,
-                                             cluster = NULL, n_cores = 1){
+                                             n_cores = 1){
 
   migration_params <- list(delta = delta, tau_ell = tau_ell, tau_n = tau_n,
                            r = r, a0 = a0, a1 = a1)
@@ -34,13 +28,8 @@ solve_equilibrium_migration_flow <- function(V_cum_list, delta, tau_ell, tau_n,
     do.call(get_migration_cum, params)
   }
 
-  if(!is.null(cluster)){
-    D_cum <- parallel::clusterApplyLB(cluster, seq_along(land_parameters_list),
-                                 get_migration_cum_i)
-  } else {
-    D_cum <- parallel::mclapply(seq_along(land_parameters_list),
+  D_cum <- parallel::mclapply(seq_along(land_parameters_list),
                               get_migration_cum_i, mc.cores = n_cores)
-  }
   D_flow <- lapply(D_cum, function(x) diff(c(0, x)))
   return(D_flow)
 }
