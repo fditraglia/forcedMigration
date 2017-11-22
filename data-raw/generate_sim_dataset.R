bf32035b9d43bf4c55f41050fb3003c0ac14dba8
Francis DiTraglia <fditra@sas.upenn.edu>
2017-11-22 14:58:36 -0500

    change simulation parameters: a1 = 0 and no targeting

--- data-raw/generate_sim_dataset.R
+++ data-raw/generate_sim_dataset.R
@@ -5,10 +5,10 @@ set.seed(1234)
 # Set parameters of simulation
 model <- list(delta = 0.2,
               tau_ell = 0.45,
-              tau_n = 0.25,
+              tau_n = 0.45,
               r = 0.1,
               a0 = 2,
-              a1 = 1)
+              a1 = 0)
 obs <- list(dbar = 0.001,
             rho = 0.3,
             nu = seq(-0.3, 0.3, length.out = 5),
