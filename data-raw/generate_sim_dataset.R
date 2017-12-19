98b8de176076ffb5aa6d63826efab23815914049
Francis DiTraglia <fditra@sas.upenn.edu>
2017-12-19 12:28:23 -0500

    clean up simulation of displacement and add simulation of total cumulative violence

--- data-raw/generate_sim_dataset.R
+++ data-raw/generate_sim_dataset.R
@@ -2,38 +2,81 @@ library(forcedMigration)
 
 set.seed(1234)
 
-# Set parameters of simulation
-model <- list(delta = 10000,
-              tau_ell = 0.5,
-              tau_n = 0.3,
-              r = 0.4,
-              a0 = 2,
-              a1 = 0)
-obs <- list(dbar = 0.001,
-            rho = 0.3,
-            nu = seq(-0.3, 0.3, length.out = 5),
-            eta = seq(-0.1, 0.1, length.out = 16)) # One fewer than T
-sim_params <- list(model = model, obs = obs)
-rm(model, obs)
-
-# Pump-priming run
+#----------------------- Pump-priming run
 get_migration_flow_i(275, sim_params$model)
 
-# Solve model at given values for structural parameters
-f <- function(i) get_migration_flow_i(i, sim_params$model)
-sim_dstar <- do.call(rbind, lapply(1:nrow(Vcum), f))
-colnames(sim_dstar) <- colnames(Vcum)
-rownames(sim_dstar) <- rownames(Vcum)
+#----------------------- Solve for eq. migration given observed violence flows
+par_D_outer <- list(delta = 10000,
+                tau_ell = 0.5,
+                tau_n = 0.3,
+                r = 0.4,
+                a0 = 2,
+                a1 = 0)
+
+dstar <- do.call(rbind, lapply(1:nrow(Vcum),
+                               function(i) get_migration_flow_i(i, par_D_outer)))
+colnames(dstar) <- colnames(Vcum)
+rownames(dstar) <- rownames(Vcum)
+
+#----------------------- Simulate observed displacement measures
+par_D_inner <- list(dbar = 0.001,
+                rho = 0.3,
+                nu = seq(-0.3, 0.3, length.out = 5),
+                eta = seq(-0.1, 0.1, length.out = 16)) # One fewer than T
+
 dstar_lag <- cbind(rep(0, nrow(sim_dstar)), sim_dstar[,-ncol(sim_dstar)])
-dstar_friction <- with(sim_params$obs, (1 - rho) * sim_dstar + rho * dstar_lag)
+true_displacement <- with(par_D_inner, dbar + (1 - rho) * sim_dstar + rho * dstar_lag)
 
-# Simulate from observation model
-pop <- cross_section$popn1993
-mu <- with(sim_params$obs,
-           (pop * t(t(dstar_friction + dbar) * exp(c(0, eta)))) %o% exp(nu))
-simZ <- array(rpois(length(mu), lambda = mu), dim = dim(mu))
+mu <- with(par_D_inner, (cross_section$popn1993 * t(t(true_displacement) *
+                                                      exp(c(0, eta)))) %o% exp(nu))
+Z <- array(rpois(length(mu), lambda = mu), dim = dim(mu))
 
-devtools::use_data(simZ, overwrite = TRUE)
-devtools::use_data(sim_params, overwrite = TRUE)
-devtools::use_data(sim_dstar, overwrite = TRUE)
+#----------------------- Solve for payoffs to contract (Dstar, Xstar, Dmax, Xmax)
+payoffs <- do.call(get_payoffs_list, c(par_D_outer, n_cores = 8))
+
+#----------------------- Solve for lambda_star and S_e_star at true contract parameters
+par_V_outer <- list(gamma = 0.2, alpha = 0.5)
+contracts <- do.call(get_contracts, c(par_V_outer, list(payoffs = payoffs)))
+
+#----------------------- Simulate fixed costs of contract (Delta)
+covariates <- mvtnorm::rmvnorm(nrow(contracts),
+                               sigma = toeplitz(c(1, seq(-0.15, 0.15, 0.05))))
+
+# Note: our convention is that Delta = S_e_star - x'beta
+X <- cbind(contracts$S_e, rep(-1, nrow(contracts)), -1 * covariates)
+scaling <- IQR(contracts$S_e) * sqrt(3) / pi
+beta <- (1 / scaling) *  c(1, -median(contracts$S_e), seq(-1, 1, length.out = ncol(covariates)))
+y <- X %*% beta
+
+#------------------------ Generate simulated violence from optimal contracts
+# Function to draw from zero-truncated poisson
+rtpois <- function(N, lambda) {
+  qpois(runif(N, dpois(0, lambda), 1), lambda)
+}
+
+Vstar <- rtpois(nrow(contracts), contracts$lam)
+logit_errors <- rlogis(nrow(contracts))
+contract_indicator <- y > logit_errors
+Vobs <- drop(contract_indicator * Vstar)
+
+#------------------------- Store simulated data and parameters
+simulation <- list(par_D_inner = par_D_inner,
+                   par_D_outer = par_D_outer,
+                   dstar = dstar,
+                   dstar_lag = dstar_lag,
+                   true_displacement = true_displacement,
+                   Z = Z,
+                   payoffs = payoffs,
+                   contracts = contracts,
+                   covariates = covariates,
+                   X = X,
+                   y = y,
+                   Vstar = Vstar,
+                   logit_errors = logit_errors,
+                   contract_indicator = contract_indicator,
+                   Vobs = Vobs)
+
+devtools::use_data(simulation, overwrite = TRUE)
+
+#----------------------- Clean up
 rm(list = ls())
