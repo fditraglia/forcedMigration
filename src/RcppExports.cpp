8ad0da31b03d92d947fe6d5231a09abdd80fb63b
Francis DiTraglia <fditra@sas.upenn.edu>
2018-03-10 14:21:28 -0500

    build package

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -9,7 +9,7 @@ using namespace Rcpp;
 
 // get_Q
 double get_Q(double V, double D_e, double delta);
-RcppExport SEXP _forcedMigration_get_Q(SEXP VSEXP, SEXP D_eSEXP, SEXP deltaSEXP) {
+RcppExport SEXP forcedMigration_get_Q(SEXP VSEXP, SEXP D_eSEXP, SEXP deltaSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -22,7 +22,7 @@ END_RCPP
 }
 // get_Dstar
 double get_Dstar(double D_e, double V, double delta, double tau_ell, double tau_n, double r, double a0, double a1, double p, double q, double H_bar, double omega_n);
-RcppExport SEXP _forcedMigration_get_Dstar(SEXP D_eSEXP, SEXP VSEXP, SEXP deltaSEXP, SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
+RcppExport SEXP forcedMigration_get_Dstar(SEXP D_eSEXP, SEXP VSEXP, SEXP deltaSEXP, SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -44,7 +44,7 @@ END_RCPP
 }
 // get_migration_eq
 double get_migration_eq(double V, double start, double delta, double tau_ell, double tau_n, double r, double a0, double a1, double p, double q, double H_bar, double omega_n);
-RcppExport SEXP _forcedMigration_get_migration_eq(SEXP VSEXP, SEXP startSEXP, SEXP deltaSEXP, SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
+RcppExport SEXP forcedMigration_get_migration_eq(SEXP VSEXP, SEXP startSEXP, SEXP deltaSEXP, SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -66,7 +66,7 @@ END_RCPP
 }
 // get_migration_cum
 NumericVector get_migration_cum(NumericVector V_cum, double delta, double tau_ell, double tau_n, double r, double a0, double a1, double p, double q, double H_bar, double omega_n);
-RcppExport SEXP _forcedMigration_get_migration_cum(SEXP V_cumSEXP, SEXP deltaSEXP, SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
+RcppExport SEXP forcedMigration_get_migration_cum(SEXP V_cumSEXP, SEXP deltaSEXP, SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -87,7 +87,7 @@ END_RCPP
 }
 // get_X
 double get_X(double Q, double tau_ell, double r, double a0, double a1, double p, double q, double H_bar, double omega_n);
-RcppExport SEXP _forcedMigration_get_X(SEXP QSEXP, SEXP tau_ellSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
+RcppExport SEXP forcedMigration_get_X(SEXP QSEXP, SEXP tau_ellSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -106,7 +106,7 @@ END_RCPP
 }
 // get_X_max
 double get_X_max(double tau_ell, double r, double a0, double a1, double p, double q, double H_bar, double omega_n);
-RcppExport SEXP _forcedMigration_get_X_max(SEXP tau_ellSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
+RcppExport SEXP forcedMigration_get_X_max(SEXP tau_ellSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -124,7 +124,7 @@ END_RCPP
 }
 // get_D_max
 double get_D_max(double tau_ell, double tau_n, double r, double a0, double a1, double p, double q, double H_bar, double omega_n);
-RcppExport SEXP _forcedMigration_get_D_max(SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
+RcppExport SEXP forcedMigration_get_D_max(SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -143,7 +143,7 @@ END_RCPP
 }
 // get_payoffs
 List get_payoffs(double delta, double tau_ell, double tau_n, double r, double a0, double a1, double p, double q, double H_bar, double omega_n, double popn);
-RcppExport SEXP _forcedMigration_get_payoffs(SEXP deltaSEXP, SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP, SEXP popnSEXP) {
+RcppExport SEXP forcedMigration_get_payoffs(SEXP deltaSEXP, SEXP tau_ellSEXP, SEXP tau_nSEXP, SEXP rSEXP, SEXP a0SEXP, SEXP a1SEXP, SEXP pSEXP, SEXP qSEXP, SEXP H_barSEXP, SEXP omega_nSEXP, SEXP popnSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -164,7 +164,7 @@ END_RCPP
 }
 // get_S
 List get_S(arma::mat X_star, arma::vec y_star, arma::mat Q, arma::vec gamma);
-RcppExport SEXP _forcedMigration_get_S(SEXP X_starSEXP, SEXP y_starSEXP, SEXP QSEXP, SEXP gammaSEXP) {
+RcppExport SEXP forcedMigration_get_S(SEXP X_starSEXP, SEXP y_starSEXP, SEXP QSEXP, SEXP gammaSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -178,15 +178,15 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_forcedMigration_get_Q", (DL_FUNC) &_forcedMigration_get_Q, 3},
-    {"_forcedMigration_get_Dstar", (DL_FUNC) &_forcedMigration_get_Dstar, 12},
-    {"_forcedMigration_get_migration_eq", (DL_FUNC) &_forcedMigration_get_migration_eq, 12},
-    {"_forcedMigration_get_migration_cum", (DL_FUNC) &_forcedMigration_get_migration_cum, 11},
-    {"_forcedMigration_get_X", (DL_FUNC) &_forcedMigration_get_X, 9},
-    {"_forcedMigration_get_X_max", (DL_FUNC) &_forcedMigration_get_X_max, 8},
-    {"_forcedMigration_get_D_max", (DL_FUNC) &_forcedMigration_get_D_max, 9},
-    {"_forcedMigration_get_payoffs", (DL_FUNC) &_forcedMigration_get_payoffs, 11},
-    {"_forcedMigration_get_S", (DL_FUNC) &_forcedMigration_get_S, 4},
+    {"forcedMigration_get_Q", (DL_FUNC) &forcedMigration_get_Q, 3},
+    {"forcedMigration_get_Dstar", (DL_FUNC) &forcedMigration_get_Dstar, 12},
+    {"forcedMigration_get_migration_eq", (DL_FUNC) &forcedMigration_get_migration_eq, 12},
+    {"forcedMigration_get_migration_cum", (DL_FUNC) &forcedMigration_get_migration_cum, 11},
+    {"forcedMigration_get_X", (DL_FUNC) &forcedMigration_get_X, 9},
+    {"forcedMigration_get_X_max", (DL_FUNC) &forcedMigration_get_X_max, 8},
+    {"forcedMigration_get_D_max", (DL_FUNC) &forcedMigration_get_D_max, 9},
+    {"forcedMigration_get_payoffs", (DL_FUNC) &forcedMigration_get_payoffs, 11},
+    {"forcedMigration_get_S", (DL_FUNC) &forcedMigration_get_S, 4},
     {NULL, NULL, 0}
 };
 
