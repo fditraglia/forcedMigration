4bd427a695a95a5c88fe35481be334d7237ffc6d
Francis DiTraglia <francis.ditraglia@economics.ox.ac.uk>
2021-02-02 11:50:50 +0000

    add testing/benchmark example

--- src/get_tanh_fixed_point.cpp
+++ src/get_tanh_fixed_point.cpp
@@ -19,10 +19,24 @@ double get_tanh_fixed_point_cpp(NumericVector w, NumericVector a, double b,
 }
 
 /*** R
-w <- c(0.6, 0.3, 0.1)
-a <- c(0, 0.2, 0.7)
-b <- 0.8
-get_tanh_fixed_point(w, a, b)
-get_tanh_fixed_point_cpp(w, a, b)
+# This example is based on Municipality 13670 (with rounding)
+Vcum <- 121
+nFam <- 3170
+w <- c(0.83, 0.029, 0.0202, 0.0177, 0.0271, 0.0183, 0.0139, 0.0268, 0.0095,
+       0.0044, 0.0025, 6e-04, 0, 0)
+
+h <- c(0, 0.35, 1.93, 3.91, 7.62, 12.49, 17.74, 30.25, 71.57, 147.95, 294.79,
+       795.24, 0, 0)
+tau <- 2.5
+r <- 0.05
+J <- 1
+a <- tau * (Vcum / nFam) - r * h
+(1 + sum(w * tanh(a))) / 2 # Fraction who leave with no social interactions (J=0)
+
+plot_tanh_fixed_point(w, a, J)
+get_tanh_fixed_point(w, a, J)
+get_tanh_fixed_point_cpp(w, a, J)
+microbenchmark::microbenchmark(get_tanh_fixed_point(w, a, J),
+                               get_tanh_fixed_point_cpp(w, a, J))
 */
 
