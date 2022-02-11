7ce25d12338e2d9bea8d406e316a479179cc3774
parkersnipes <psnipes1@gmail.com>
2022-02-11 01:59:21 -0600

    Update plottingFunctions.R

--- R/plottingFunctions.R
+++ R/plottingFunctions.R
@@ -335,7 +335,7 @@ violence_map <- function(year,covariate){
    tryCatch({
     map_with_attributes <-  subset(forcedMigration::panel[forcedMigration::panel$year == year,],select = c("municipality",covariate))
    },{
-   error = function(cond) <- {print("Please enter year between 1996 and 2008 and one of the following covariates: V_cum, V_flow, D_AS, D_CODHES, D_RUV, D_CEDE, D_JYP, lag_V_flow.")}
+   error = function(cond) {print("Please enter year between 1996 and 2008 and one of the following covariates: V_cum, V_flow, D_AS, D_CODHES, D_RUV, D_CEDE, D_JYP, lag_V_flow.")}
    })
    
    case_when(
